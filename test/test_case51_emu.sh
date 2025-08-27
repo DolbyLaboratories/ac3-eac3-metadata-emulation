538f229fa30744fb9e77d7d4440ac67125a5d8df
Cowdery, James <jtc@dolby.com>
2025-08-27 08:57:45 -0400

    Added missing test source file

--- test/test_case51_emu.sh
+++ test/test_case51_emu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo File: $1 MdEmu Config: $2
+echo File: $1 Acmod: $2 Dialnorm: $3 Profile: $4 Compression Mode: $5
 export stem=`basename $1 .wav`
 
-build_debug/MdEmu $2 $1 test/output/$stem.emu.wav
+build_debug/MdEmu -p0 -s0 -a$2 -dn$3 -k$4 -c$5 $1 test/output/$stem.emu.wav
 
-export pwt=./tools/pwr_vs_time
+export pwt=pwr_vs_time
 
 $pwt -c 0 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.0.res
 $pwt -c 1 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.1.res
 $pwt -c 2 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.2.res
-$pwt -c 4 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.3.res
-$pwt -c 5 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.4.res
+$pwt -c 4 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.4.res
+$pwt -c 5 -s -i test/output/$stem.emu.wav -to test/output/$stem.emu.5.res
